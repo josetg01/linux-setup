90d4eb81985feb09f4fb28f9eb36866c319e99c0
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-12-03 00:42:49 +0100

    Update openvash-ubuntu.sh

--- openvash-ubuntu.sh
+++ openvash-ubuntu.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 
 sudo useradd -r -M -U -G sudo -s /usr/sbin/nologin gvm
 sudo usermod -aG gvm $USER
@@ -57,3 +59,77 @@ sudo cp -rv $INSTALL_DIR/* /
 
 #GVMD
 export GVMD_VERSION=$GVM_VERSION
+sudo apt install -y \
+  libglib2.0-dev \
+  libgnutls28-dev \
+  libpq-dev \
+  postgresql-server-dev-13 \
+  libical-dev \
+  xsltproc \
+  rsync \
+  libbsd-dev \
+  libgpgme-dev
+sudo apt install -y --no-install-recommends \
+  texlive-latex-extra \
+  texlive-fonts-recommended \
+  xmlstarlet \
+  zip \
+  rpm \
+  fakeroot \
+  dpkg \
+  nsis \
+  gnupg \
+  gpgsm \
+  wget \
+  sshpass \
+  openssh-client \
+  socat \
+  snmp \
+  python3 \
+  smbclient \
+  python3-lxml \
+  gnutls-bin \
+  xml-twig-tools
+curl -f -L https://github.com/greenbone/gvmd/archive/refs/tags/v$GVMD_VERSION.tar.gz -o $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/gvmd/releases/download/v$GVMD_VERSION/gvmd-$GVMD_VERSION.tar.gz.asc -o $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz.asc
+gpg --verify $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz.asc $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvmd-$GVMD_VERSION.tar.gz
+mkdir -p $BUILD_DIR/gvmd && cd $BUILD_DIR/gvmd
+
+cmake $SOURCE_DIR/gvmd-$GVMD_VERSION \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLOCALSTATEDIR=/var \
+  -DSYSCONFDIR=/etc \
+  -DGVM_DATA_DIR=/var \
+  -DGVMD_RUN_DIR=/run/gvmd \
+  -DOPENVAS_DEFAULT_SOCKET=/run/ospd/ospd-openvas.sock \
+  -DGVM_FEED_LOCK_PATH=/var/lib/gvm/feed-update.lock \
+  -DSYSTEMD_SERVICE_DIR=/lib/systemd/system \
+  -DLOGROTATE_DIR=/etc/logrotate.d
+
+make -j$(nproc)
+make DESTDIR=$INSTALL_DIR install
+
+sudo cp -rv $INSTALL_DIR/* /
+#pg-gvm
+export PG_GVM_VERSION=$GVM_VERSION
+sudo apt install -y \
+  libglib2.0-dev \
+  postgresql-server-dev-13 \
+  libical-dev
+curl -f -L https://github.com/greenbone/pg-gvm/archive/refs/tags/v$PG_GVM_VERSION.tar.gz -o $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz
+curl -f -L https://github.com/greenbone/pg-gvm/releases/download/v$PG_GVM_VERSION/pg-gvm-$PG_GVM_VERSION.tar.gz.asc -o $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz.asc
+gpg --verify $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz.asc $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz
+tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION.tar.gz
+mkdir -p $BUILD_DIR/pg-gvm && cd $BUILD_DIR/pg-gvm
+
+cmake $SOURCE_DIR/pg-gvm-$PG_GVM_VERSION \
+  -DCMAKE_BUILD_TYPE=Release
+
+make -j$(nproc)
+make DESTDIR=$INSTALL_DIR install
+
+sudo cp -rv $INSTALL_DIR/* /
+
+#GSA
