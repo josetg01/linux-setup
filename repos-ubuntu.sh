35b1af9188b2b11825a18473b9052dcda66ceaa4
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-12-03 21:00:23 +0100

    Update repos-ubuntu.sh

--- repos-ubuntu.sh
+++ repos-ubuntu.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
+sudo apt update
+
 #Brave Browser
-sudo apt install -y apt-transport-https curl
+sudo apt install -y apt-transport-https curl ca-certificates
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
