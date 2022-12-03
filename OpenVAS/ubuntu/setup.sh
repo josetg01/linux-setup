26cb89e91aa58e9fbd6c7f6d8e0708758937d95a
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-12-03 10:52:53 +0100

    Update setup.sh

--- OpenVAS/ubuntu/setup.sh
+++ OpenVAS/ubuntu/setup.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/prerequisitos.sh | bash
 curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/build-install-components.sh | bash
-
+curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/perform-system-setup.sh | bash
