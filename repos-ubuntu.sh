87c5086b31615af49bae4d04b603c3df22732efd
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-12-03 20:59:38 +0100

    Update repos-ubuntu.sh

--- repos-ubuntu.sh
+++ repos-ubuntu.sh
@@ -23,6 +23,8 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 #VirtualBox
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian focal contrib' | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 
 #Kernel Xanmod
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
