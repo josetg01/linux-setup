73c4fae87dca4d4adbbc856e3ea76f1f19c98b79
Jose Maria Torregrosa Gomariz <j.m.torregros@gmail.com>
2022-12-03 01:06:01 +0100

    q

---
+++ OpenVAS/ubuntu/setup.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/prerequisitos.sh | bash
+curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/build-install-components.sh | bash
+
