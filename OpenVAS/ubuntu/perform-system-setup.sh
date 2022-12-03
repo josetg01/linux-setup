49f5fe4557c6c0d81bc74c63d907c97a4aaeadf3
Jose Maria Torregrosa Gomariz <j.m.torregros@gmail.com>
2022-12-03 10:51:16 +0100

    Performing a System Setup finish

--- OpenVAS/ubuntu/perform-system-setup.sh
+++ OpenVAS/ubuntu/perform-system-setup.sh
@@ -1,1 +1,173 @@
 #/bin/bash
+
+#Setting up the Redis Data Store
+sudo apt install -y redis-server
+sudo cp $SOURCE_DIR/openvas-scanner-$GVM_VERSION/config/redis-openvas.conf /etc/redis/
+sudo chown redis:redis /etc/redis/redis-openvas.conf
+echo "db_address = /run/redis-openvas/redis.sock" | sudo tee -a /etc/openvas/openvas.conf
+sudo systemctl start redis-server@openvas.service
+sudo systemctl enable redis-server@openvas.service
+sudo usermod -aG redis gvm
+
+#Setting up the Mosquitto MQTT Broker
+sudo apt install -y mosquitto
+sudo systemctl start mosquitto.service
+sudo systemctl enable mosquitto.service
+echo "mqtt_server_uri = localhost:1883" | sudo tee -a /etc/openvas/openvas.conf
+sudo mkdir -p /var/lib/notus
+sudo mkdir -p /run/gvmd
+
+sudo chown -R gvm:gvm /var/lib/gvm
+sudo chown -R gvm:gvm /var/lib/openvas
+sudo chown -R gvm:gvm /var/lib/notus
+sudo chown -R gvm:gvm /var/log/gvm
+sudo chown -R gvm:gvm /run/gvmd
+
+sudo chmod -R g+srw /var/lib/gvm
+sudo chmod -R g+srw /var/lib/openvas
+sudo chmod -R g+srw /var/log/gvm
+sudo chown gvm:gvm /usr/local/sbin/gvmd
+sudo chmod 6750 /usr/local/sbin/gvmd
+sudo chown gvm:gvm /usr/local/bin/greenbone-nvt-sync
+sudo chmod 740 /usr/local/sbin/greenbone-feed-sync
+sudo chown gvm:gvm /usr/local/sbin/greenbone-*-sync
+sudo chmod 740 /usr/local/sbin/greenbone-*-sync
+
+#Feed Validation
+export GNUPGHOME=/tmp/openvas-gnupg
+mkdir -p $GNUPGHOME
+
+gpg --import /tmp/GBCommunitySigningKey.asc
+gpg --import-ownertrust < /tmp/ownertrust.txt
+
+export OPENVAS_GNUPG_HOME=/etc/openvas/gnupg
+sudo mkdir -p $OPENVAS_GNUPG_HOME
+sudo cp -r /tmp/openvas-gnupg/* $OPENVAS_GNUPG_HOME/
+sudo chown -R gvm:gvm $OPENVAS_GNUPG_HOME
+
+#Setting up sudo for Scanning
+cat >> /etc/sudoers  <<EOL
+...
+
+# allow users of the gvm group run openvas
+%gvm ALL = NOPASSWD: /usr/local/sbin/openvas
+EOL
+
+#Setting up PostgreSQL
+sudo apt install -y postgresql
+sudo systemctl start postgresql@13-main
+sudo -u postgres bash
+createuser -DRS gvm
+createdb -O gvm gvmd
+exit
+sudo -u postgres bash
+psql gvmd
+create role dba with superuser noinherit;
+grant dba to gvm;
+
+exit
+exit
+
+#Setting up an Admin User
+gvmd --create-user=admin --password=admin
+
+#Setting the Feed Import Owner
+gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value `gvmd --get-users --verbose | grep admin | awk '{print $2}'`
+cat << EOF > $BUILD_DIR/ospd-openvas.service
+[Unit]
+Description=OSPd Wrapper for the OpenVAS Scanner (ospd-openvas)
+Documentation=man:ospd-openvas(8) man:openvas(8)
+After=network.target networking.service redis-server@openvas.service
+Wants=redis-server@openvas.service
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+RuntimeDirectory=ospd
+RuntimeDirectoryMode=2775
+PIDFile=/run/ospd/ospd-openvas.pid
+ExecStart=/usr/local/bin/ospd-openvas --unix-socket /run/ospd/ospd-openvas.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas --socket-mode 0o770 --mqtt-broker-address localhost --mqtt-broker-port 1883 --notus-feed-dir /var/lib/notus/advisories
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/ospd-openvas.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/notus-scanner.service
+[Unit]
+Description=Notus Scanner
+Documentation=https://github.com/greenbone/notus-scanner
+After=mosquitto.service
+Wants=mosquitto.service
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+RuntimeDirectory=notus-scanner
+RuntimeDirectoryMode=2775
+PIDFile=/run/notus-scanner/notus-scanner.pid
+ExecStart=/usr/local/bin/notus-scanner --products-directory /var/lib/notus/products --log-file /var/log/gvm/notus-scanner.log
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/notus-scanner.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/gvmd.service
+[Unit]
+Description=Greenbone Vulnerability Manager daemon (gvmd)
+After=network.target networking.service postgresql.service ospd-openvas.service
+Wants=postgresql.service ospd-openvas.service
+Documentation=man:gvmd(8)
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+PIDFile=/run/gvmd/gvmd.pid
+RuntimeDirectory=gvmd
+RuntimeDirectoryMode=2775
+ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/gsad.service
+[Unit]
+Description=Greenbone Security Assistant daemon (gsad)
+Documentation=man:gsad(8) https://www.greenbone.net
+After=network.target gvmd.service
+Wants=gvmd.service
+
+[Service]
+Type=exec
+User=gvm
+Group=gvm
+RuntimeDirectory=gsad
+RuntimeDirectoryMode=2775
+PIDFile=/run/gsad/gsad.pid
+ExecStart=/usr/local/sbin/gsad --foreground --listen=127.0.0.1 --port=9392 --http-only
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+Alias=greenbone-security-assistant.service
+EOF
+sudo cp $BUILD_DIR/gsad.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable notus-scanner
+sudo systemctl enable ospd-openvas
+sudo systemctl enable gvmd
+sudo systemctl enable gsad
