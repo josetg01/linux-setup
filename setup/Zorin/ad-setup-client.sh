057f2853940a65f69a75ba0845023a2684869ae9
josetg01 <35702013+josetg01@users.noreply.github.com>
2023-12-05 12:00:55 +0100

    Update ad-setup-client.sh

--- setup/Zorin/ad-setup-client.sh
+++ setup/Zorin/ad-setup-client.sh
@@ -1,6 +1,9 @@
 #/bin/bash
-
+read -p "Nombre de equipo: " nombrepc
+read -p "Dominio Windows: " dominio
 #Instalacion paquetes necesarios
 sudo apt update && sudo apt upgrade -y
 apt install -y realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 
+#
+sudo hostnamectl set-hostname $nombrepc.$dominio
