6fa093d3693015025de06dabc2555db8361a3f42
josetg01 <35702013+josetg01@users.noreply.github.com>
2023-12-06 11:24:43 +0100

    Update ad-setup-client.sh

--- setup/Zorin/ad-setup-client.sh
+++ setup/Zorin/ad-setup-client.sh
@@ -2,7 +2,7 @@
 read -p "Nombre de equipo: " nombrepc
 read -p "Dominio Windows: " dominio
 read -p "Usuario para unir al dominio: " userdomain
-read -s -p "Contraseña del usuario para unir al dominio" passwduserdomain
+read -s -p "Contraseña del usuario para unir al dominio: " passwduserdomain
 #Instalacion paquetes necesarios
 sudo apt update && sudo apt upgrade -y
 apt install -y realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
