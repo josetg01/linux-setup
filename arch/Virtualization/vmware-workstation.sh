4af78e78443338ef4332e7e5c0e4dba152d29188
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-02-19 11:00:22 +0100

    Update vmware-workstation.sh

--- arch/Virtualization/vmware-workstation.sh
+++ arch/Virtualization/vmware-workstation.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+
+yay -Sy vmware-workstation
+# Detectar el tipo de kernel instalado
+if pacman -Q | grep -q linux-zen; then
+    echo "Kernel Zen detectado."
+    KERNEL_TYPE="zen"
+elif pacman -Q | grep -q linux-lts; then
+    echo "Kernel LTS detectado."
+    KERNEL_TYPE="lts"
+else
+    echo "Kernel genérico detectado."
+    KERNEL_TYPE="generic"
+fi
+
+# Instalar los encabezados correspondientes al kernel
+if [ "$KERNEL_TYPE" == "zen" ]; then
+    sudo pacman -S linux-zen-headers
+elif [ "$KERNEL_TYPE" == "lts" ]; then
+    sudo pacman -S linux-lts-headers
+else
+    sudo pacman -S linux-headers
+    
+fi
+sudo modprobe -a vmw_vmci vmmon
+sudo systemctl enable vmware-networks
+sudo systemctl enable
