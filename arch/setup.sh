9f17f6d0b36e8c0005877051c698c8d165993607
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-02-19 10:43:43 +0100

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -2,3 +2,20 @@
 
 #
 sudo pacman -Sy --noconfirm git base-devel
+
+if command -v yay &> /dev/null; then
+    echo "yay ya está instalado."
+else
+    echo "yay no está instalado. Iniciando la instalación..."
+
+    # Instalar yay desde AUR
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+
+    # Limpiar el directorio temporal
+    cd ..
+    rm -rf yay
+
+    echo "yay ha sido instalado correctamente."
+fi
