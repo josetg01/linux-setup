39946da80323148f53570db071661e3056812fcc
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-05-11 10:19:45 +0200

    Delete OpenVAS directory

--- OpenVAS/ubuntu/setup.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/prerequisitos.sh | bash
-curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/build-install-components.sh | bash
-curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/perform-system-setup.sh | bash
-curl https://raw.githubusercontent.com/josetg01/linux-setup/main/OpenVAS/ubuntu/perform-feed-sync.sh | bash
