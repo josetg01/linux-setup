5793cd8349f5c140f6514aea5ed6fb12571fe80a
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-05-11 09:54:24 +0200

    Delete arch/kde.sh

--- arch/kde.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -Sy
-function plasma () {
-sudo pacman -S --noconfirm bluedevilk breeze-gtk discover drkonqi kde-gtk-config kdeplasma-addons kgamma kinfocenter kscreen ksshaskpass kwallet-pam kwrited ocean-sound-theme oxygen oxygen-sounds plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-nm plasma-pa plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-welcome plasma-workspace-wallpapers powerdevil print-manager sddm-kcm xdg-desktop-portal-kde
-}
