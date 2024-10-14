968d1a807811b984cf06d1aaa678836b0c1e638d
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-10-14 18:54:12 +0200

    Update join-ldap.sh

--- proyecto/join-ldap.sh
+++ proyecto/join-ldap.sh
@@ -1,5 +1,28 @@
-#/bin/bash
+#!/bin/bash
+
+# Verifica que se ha pasado un argumento
+if [ "$#" -ne 1 ]; then
+    echo "Uso: $0 dominio"
+    exit 1
+fi
+
+# Lee el dominio
+dominio="$1"
+
+# Divide el dominio en partes utilizando el punto como delimitador
+IFS='.' read -r -a partes <<< "$dominio"
+
+# Inicializa la variable LDAP
+ldap_format=""
+
+# Recorre las partes y las transforma al formato LDAP
+for parte in "${partes[@]}"; do
+    ldap_format+="dc=${parte},"
+done
+
+# Elimina la última coma
+ldap_format="${ldap_format%,}"
 
 #Editamos el fichero /etc/ldap.conf
-sed -i 's/BASE.*/BASE\t '
-sed -i 's/URI.*/URI\tldap://'
+sed -i 's/BASE.*/BASE\t$ldap_format'
+sed -i 's/URI.*/URI\tldap://ldap.$dominio'
