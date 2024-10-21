4c78ba0ba7f5313691ed2a7ebf3b5baf9a2e2ecf
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-10-21 19:35:44 +0200

    Update modify-ldap.sh

--- proyecto/modify-ldap.sh
+++ proyecto/modify-ldap.sh
@@ -33,10 +33,22 @@ añadir_usuario(){
   echo "La contraseña del usuario es: $password"
   exit 0
 }
-calc_gid(){}
+calc_gid(){
+  # Obtener el GID máximo actual
+  max_gid=$(ldapsearch -x -LLL -D "$BIND_DN" -w "$BIND_PASSWD" -b "$BASE_DN" "(objectClass=posixGroup)" gidNumber | grep gidNumber | awk '{print $2}' | sort -n | tail -n 1)
+
+  # Si no hay GIDs, empezar desde 1000 (puedes ajustar esto según tus necesidades)
+  if [ -z "$max_gid" ]; then
+    new_gid=1000
+  else
+    new_gid=$((max_gid + 1))
+  fi
+}
 añadir_grupo(){
   read -p "Nombre de grupo: " nomgroup
+  new_gid=$(calc_gid)
   echo "El nombre del grupo es: $nomgroup"
+  echo "El numero de gid es: $new_gid"
   exit
 }
 añadir_uo(){
