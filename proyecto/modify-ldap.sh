aa6cfa8d90a36895bd687d0836c155940b845284
josetg01 <35702013+josetg01@users.noreply.github.com>
2024-10-21 17:45:47 +0200

    Update modify-ldap.sh

--- proyecto/modify-ldap.sh
+++ proyecto/modify-ldap.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Configuración de variables
-LDAP_SERVER="locahost"
+LDAP_SERVER="localhost"
 BASE_DN="dc=josemaria,dc=local"
 BIND_DN="cn=admin,$BASE_DN"
 BIND_PASSWD="alumno"
 
-
 #
+añadir_objecto(){
+
+}
+añadir_usuario(){}
+añadir_grupo(){}
+añadir_uo(){}
